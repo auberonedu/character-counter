1ad1e91622b10d03adf3e2d77647720b6f48b18f
Alex_Borovets <Borovale002@gmail.com>
2025-01-20 16:29:13 -0800

    Changes have been made and it works flawlessly

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,26 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    public void testAppearsThreeTimesInHelloWord(){
+        //Arrange
+        CharacterFrequencyCounter counter= new CharacterFrequencyCounter("Hello world");
+
+        //Act
+        int actualAcout = counter.getFrequency('o');
+
+        //Assert
+        assertEquals(2,actualAcout);
+    }
+    @Test
+    public void testNoneOccuringCharactersPercentsgeIsZero(){
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hi there");
+
+        //Act 
+        double actualPercentage = counter.getRelativePercentage('z');
+
+        //Assert 
+        assertEquals(0.0, actualPercentage, 0.0001);
+    }
 }
